95032bd2ab9a8608d4f3e41f32cf7f6e4cbe0490
kmiller92718 <kmiller92718@uat.edu>
2025-04-11 15:09:45 -0400

    Assignment2.2 Keith Miller

---
+++ code.js
@@ -0,0 +1,31 @@
+// function that contains the JS for the game LowHigh
+function lowHigh() {
+  // getElementById stores the outcome of the game in results
+   let results = document.getElementById("game")
+  //  list of varibles that generate random numbers 1 - 6
+    var die1 = Math.floor(Math.random()*6)+1;
+    var die2 = Math.floor(Math.random()*6)+1;
+    var die3 = Math.floor(Math.random()*6)+1;
+    var die4 = Math.floor(Math.random()*6)+1;
+    // varibles for the 2 players
+    var compTotal;
+    var playerTotal;
+    // performs the additions for each players die
+    compTotal = die1 + die3;
+    playerTotal = die2 + die4;
+    // console.log displays the discsription of each varible and its value at the time
+    console.log("die 1 = " + die1);
+    console.log("die 2 = " + die2);
+    console.log("die 3 = " + die3);
+    console.log("die 4 = " + die4);
+    console.log("Computer = " + compTotal);
+    console.log("Player = " +playerTotal);
+    // conditional if/eles statement compares if compTotal is greater than playerTotal. Returns You Lose if true and You Win if false 
+    if (compTotal > playerTotal){
+      results.innerHTML="You Lose";
+    } else {
+      results.innerHTML="You Win";
+    }
+
+    
+}
