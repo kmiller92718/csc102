3d88f662a4053281a02c81179283b9b3293f8a1f
kmiller92718 <kmiller92718@uat.edu>
2025-05-03 09:18:09 -0400

    Assignment 5.1: Final Project

--- code.js
+++ code.js
@@ -13,18 +13,20 @@ function lowHigh() {
     compTotal = die1 + die3;
     playerTotal = die2 + die4;
     // console.log displays the discsription of each varible and its value at the time
-    console.log("die 1 = " + die1);
-    console.log("die 2 = " + die2);
-    console.log("die 3 = " + die3);
-    console.log("die 4 = " + die4);
-    console.log("Computer = " + compTotal);
-    console.log("Player = " +playerTotal);
+    
+    
     // conditional if/eles statement compares if compTotal is greater than playerTotal. Returns You Lose if true and You Win if false 
     if (compTotal > playerTotal){
       document.getElementById("game").innerHTML="You Lose"
-    } else {
+    } else if (compTotal == playerTotal){
+      document.getElementById("game").innerHTML="Push"
+    } else{
       document.getElementById("game").innerHTML="You Win"
     }
-
-    
+    document.getElementById("die1").innerHTML= die1;
+    document.getElementById("die2").innerHTML = die2;
+    document.getElementById("die3").innerHTML= die3;
+    document.getElementById("die4").innerHTML = die4;
+    document.getElementById("compTotal").innerHTML = compTotal;
+    document.getElementById("playerTotal").innerHTML = playerTotal;
 }
